4cc24295ee4971b762e98fb72a9bcd7ea0780ef5
Rutuja Bhojane <rutujabhojane14@gmail.com>
2025-08-05 13:03:32 +0530

    Maintained history and recent searches

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,10 @@ import Answers from "./components/Answers";
 function App() {
   const [question, setQuestion] = useState("");
   const [result, setResult] = useState([]);
+  const [recentHistory, setRecentHistory] = useState(
+    JSON.parse(localStorage.getItem("history"))
+  );
+
   const payload = {
     contents: [
       {
@@ -18,6 +22,15 @@ function App() {
     ],
   };
   const askQuestion = async () => {
+    if (localStorage.getItem("history")) {
+      let history = JSON.parse(localStorage.getItem("history"));
+      history = [question, ...history];
+      localStorage.setItem("history", JSON.stringify(history));
+      setRecentHistory(history);
+    } else {
+      localStorage.setItem("history", JSON.stringify([question]));
+      setRecentHistory([question]);
+    }
     let response = await fetch(URL, {
       method: "POST",
       body: JSON.stringify(payload),
@@ -36,7 +49,9 @@ function App() {
   console.log(result);
   return (
     <div className="grid grid-cols-5 h-screen text-center">
-      <div className="col-span-1 bg-zinc-800"></div>
+      <div className="col-span-1 bg-zinc-800">
+        <ul>{recentHistory && recentHistory.map((item) => <li>{item}</li>)}</ul>
+      </div>
       <div className="col-span-4 flex flex-col h-screen p-20">
         <div className="container flex-1 overflow-auto">
           <div className="text-zinc-300">
