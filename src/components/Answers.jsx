638c6d72c4f9a8d451f90aa9c0c8670690f1d31d
Rutuja Bhojane <rutujabhojane14@gmail.com>
2025-08-12 18:28:14 +0530

    feat: add syntax highlighting for Markdown code blocks

--- src/components/Answers.jsx
+++ src/components/Answers.jsx
@@ -1,6 +1,9 @@
 import React, { useEffect, useState } from "react";
 import { checkHeading } from "../helper";
 import { replaceHeadingStars } from "../helper";
+import SyntaxHighlighter from "react-syntax-highlighter";
+import { dark } from "react-syntax-highlighter/dist/esm/styles/prism";
+import ReactMarkdown from "react-markdown";
 
 const Answers = ({ ans, index, totalResult, type }) => {
   const [heading, setHeading] = useState(false);
@@ -13,6 +16,26 @@ const Answers = ({ ans, index, totalResult, type }) => {
       setAnswer(replaceHeadingStars(ans));
     }
   }, []);
+
+  const renderer = {
+    code({ node, inline, className, children, ...props }) {
+      const match = /language-(\w+)/.exec(className || "");
+      return !inline && match ? (
+        <SyntaxHighlighter
+          {...props}
+          children={String(children).replace(/\n$/, "")}
+          language={match[1]}
+          style={dark}
+          PreTag="div"
+        />
+      ) : (
+        <code className={className} {...props}>
+          {children}
+        </code>
+      );
+    },
+  };
+
   return (
     <>
       {index === 0 && totalResult > 1 ? (
@@ -20,7 +43,9 @@ const Answers = ({ ans, index, totalResult, type }) => {
       ) : heading ? (
         <span className="pt-2 text-lg block text-white">{answer}</span>
       ) : (
-        <span className={type === "q" ? "pl-1" : "pl-5"}>{answer}</span>
+        <span className={type === "q" ? "pl-1" : "pl-5"}>
+          <ReactMarkdown components={renderer}>{answer}</ReactMarkdown>
+        </span>
       )}
     </>
   );
